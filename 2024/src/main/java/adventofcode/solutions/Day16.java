11d66be8592a68244c759fd280ce0d1f07c07977
qez008 <morten_bergmann@live.no>
2024-12-16 14:23:35 +0100

    Refactor day 16

--- 2024/src/main/java/adventofcode/solutions/Day16.java
+++ 2024/src/main/java/adventofcode/solutions/Day16.java
@@ -1,11 +1,9 @@
 package adventofcode.solutions;
 
 import adventofcode.util.IntVector2;
-import io.vavr.Tuple2;
 import lombok.RequiredArgsConstructor;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 class Day16 implements Solution {
@@ -16,12 +14,14 @@ class Day16 implements Solution {
 
     record State(IntVector2 pos, IntVector2 dir, long cost, List<IntVector2> path) {}
 
+    record PosAndDir(IntVector2 pos, IntVector2 dir) {}
+
     record Result(long cost, long seats) {}
 
     private Result cheapestPathV2() {
         var queue = new PriorityQueue<>(Comparator.comparingLong(State::cost));
         queue.add(new State(start, IntVector2.RIGHT, 0L, List.of(start)));
-        var visited = new HashMap<Tuple2<IntVector2, IntVector2>, Long>();
+        var visited = new HashMap<PosAndDir, Long>();
 
         Long best = null;
         var seats = new HashSet<IntVector2>();
@@ -37,26 +37,25 @@ class Day16 implements Solution {
                     break;
                 }
             }
-            visited.put(new Tuple2<>(pos, dir), cost);
+            visited.put(new PosAndDir(pos, dir), cost);
 
-            var next = Stream
-                    .of(dir, dir.rotateClockwise90(), dir.rotateCounterClockwise90())
-                    .map(d -> new Tuple2<>(pos.plus(d), d))
-                    .filter(t -> t.apply((p, d) -> p.getValueFrom(grid) != '#'))
-                    .toList();
+            for (var nextDir : List.of(dir, dir.rotateClockwise90(), dir.rotateCounterClockwise90())) {
+                var nextPos = pos.plus(nextDir);
+                if (nextPos.getValueFrom(grid) == '#') {
+                    continue;
+                }
+                long nextCost = cost + (nextDir.equals(dir) ? 1 : 1001);
+                var nextPath = new ArrayList<>(path);
+                nextPath.add(nextPos);
+                var state = new State(nextPos, nextDir, nextCost, nextPath);
 
-            for (var n : next) {
-                long c = cost + (n._2.equals(dir) ? 1 : 1001);
-                var p = new ArrayList<>(path);
-                p.add(n._1);
-                var state = new State(n._1, n._2, c, p);
-                Long previous = visited.get(n);
-                if (previous == null || previous > c) {
+                Long previous = visited.get(new PosAndDir(nextPos, nextDir));
+                if (previous == null || previous > nextCost) {
                     queue.add(state);
                 }
             }
         }
-        return new Result(best, seats.stream().distinct().count());
+        return new Result(best, seats.size());
     }
 
     public long part1() {
